205a25fce3c56f980917516c7e72d3a01043f7e2
Mathew Randal <mrandal0000@gmail.com>
2025-03-20 13:53:10 -0500

    setup file structure + db.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 //url: http://localhost:3000/
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = t000
 
 app.get('/', (req, res) => {
     console.log('um', req.method)
     res.sendStatus(201)
 })
 
-app.listen(PORT, () => console.log('Server has started on: ${PORT}' ))
+
+
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}` ))
