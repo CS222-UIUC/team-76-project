ccb776500555a0250c50a3e3ec69bc5ebefa5af7
Maahum Imran <mimra2@illinois.edu>
2025-04-06 14:11:52 -0500

    set up password encryption and auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@
 const app = require('./app'); // ✅ Import the Express app
 const PORT = process.env.PORT || 5000;
 
+//routes
+app.use('/auth', authRoutes)
+
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
